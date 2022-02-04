ab59848e43cb15725e157e0c9662c5126c183b60
WizzardHub <lucaslegeek69@gmail.com>
2022-02-04 16:17:06 +0100

    Added Config Model

---
+++ ProxyChecker/Models/Config.cs
@@ -0,0 +1,21 @@
+using Leaf.xNet;
+
+namespace ProxyChecker.Models
+{
+    public class Config
+    {
+        public Proxy Proxy { get; private set; }
+        public Checker Checker { get; private set; }
+    }
+
+    public class Proxy
+    {
+        public ProxyType Type { get; private set; }
+        public int TimeOut { get; private set; }
+    }
+
+    public class Checker
+    {
+        public int Threads { get; private set; }
+    }
+}
