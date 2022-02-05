f700035b22e7add03d2ca261c3ebf4b1be02e30f
WizzardHub <lucaslegeek69@gmail.com>
2022-02-05 15:46:08 +0100

    Added FileDialogUtils.cs

---
+++ ProxyChecker/Utils/FileDialogUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ProxyChecker.Utils
+{
+    public class FileDialogUtils
+    {
+        [STAThread]
+        public static string SelectFile()
+        {
+            var dialog = new OpenFileDialog
+            {
+                Multiselect = false,
+                DefaultExt = "*.txt"
+            };
+
+            using (dialog)
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    return dialog.FileName;
+                }
+            }
+            return null;
+        }
+    }
+}
