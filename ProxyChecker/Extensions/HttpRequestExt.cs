4e92c4b104034d7070f8ff1b174665e3d10590a9
WizzardHub <lucaslegeek69@gmail.com>
2022-02-07 15:55:11 +0100

    Added extension for HttpRequest

---
+++ ProxyChecker/Extensions/HttpRequestExt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Leaf.xNet;
+
+namespace ProxyChecker.Extensions
+{
+    public static class HttpRequestExt
+    {
+        public static HttpRequest UseProxy(this HttpRequest req, string proxy, ProxyType proxyType, int timeout)
+        {
+            req.Proxy = proxyType switch
+            {
+                ProxyType.HTTP => HttpProxyClient.Parse(proxy),
+                ProxyType.Socks4 => Socks4ProxyClient.Parse(proxy),
+                ProxyType.Socks4A => Socks4ProxyClient.Parse(proxy),
+                ProxyType.Socks5 => Socks4ProxyClient.Parse(proxy)
+            };
+            req.IgnoreProtocolErrors = true;
+            req.ConnectTimeout = timeout;
+            req.KeepAliveTimeout = timeout;
+            req.ReadWriteTimeout = timeout;
+            return req;
+        }
+    }
+}
