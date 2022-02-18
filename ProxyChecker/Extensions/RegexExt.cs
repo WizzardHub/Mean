0d32e289d5ca0a81b06a0a5a9e2dacec8423c1e3
WizzardHub <lucaslegeek69@gmail.com>
2022-02-18 18:53:40 +0100

    Added useful extensions to default Regex System

---
+++ ProxyChecker/Extensions/RegexExt.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ProxyChecker.Extensions
+{
+    public static class RegexExt
+    {
+
+        /*
+         * Returns the first match of a regex result
+         */
+        
+        public static Match MatchFirst(this Regex regex, string input)
+        {
+            MatchCollection result = regex.Matches(input);
+            return result[0];
+        }
+        
+        
+        /*
+         * Checks if a regex pattern is valid with the input data
+         */
+        
+        public static bool IsValidMatch(this Regex regex, string input)
+        {
+            MatchCollection result = regex.Matches(input);
+            return result.Count > 0;
+        }
+
+    }
+}
