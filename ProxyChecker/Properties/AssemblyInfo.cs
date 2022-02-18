3cb84a358af1cee22c60395431fbd7657f14247c
WizzardHub <lucaslegeek69@gmail.com>
2022-02-18 18:52:38 +0100

    1.01 Version, Edited AssemblyInfo.cs

--- ProxyChecker/Properties/AssemblyInfo.cs
+++ ProxyChecker/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ProxyChecker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Amazing C# ProxyChecker")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Wizzard LLC")]
 [assembly: AssemblyProduct("ProxyChecker")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.01")]
+[assembly: AssemblyFileVersion("1.01")]
