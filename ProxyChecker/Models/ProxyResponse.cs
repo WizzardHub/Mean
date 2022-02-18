cfff7bd3af55e8f130590c6e104b0d3cb939d170
WizzardHub <lucaslegeek69@gmail.com>
2022-02-18 18:55:13 +0100

    Added proxy saving support via a pool

---
+++ ProxyChecker/Models/ProxyResponse.cs
@@ -0,0 +1,37 @@
+﻿using MaxMind.GeoIP2.Model;
+
+namespace ProxyChecker.Models
+{
+    public class ProxyResponse
+    {
+
+        private bool _isOk;
+        private string _proxy;
+        private Country _country;
+
+        public bool IsOk
+        {
+            get => _isOk;
+            set => _isOk = value;
+        }
+        
+        public string Proxy
+        {
+            get => _proxy;
+            set => _proxy = value;
+        }
+
+        public Country Country
+        {
+            get => _country;
+            set => _country = value;
+        }
+
+        public ProxyResponse(bool isOk, string proxy, Country country = default)
+        {
+            _isOk = isOk;
+            _proxy = proxy;
+            _country = country;
+        }
+    }
+}
---
+++ ProxyChecker/Utils/FileUtils.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ProxyChecker.Models;
+
+namespace ProxyChecker.Utils
+{
+    public class FileUtils
+    {
+        public static Task SaveProxy(List<ProxyResponse> proxy)
+        {
+            
+            if (proxy.Count <= 0)
+                return Task.CompletedTask;
+
+            var currentCheckPath = Process
+                .GetCurrentProcess()
+                .StartTime
+                .ToString("R")
+                .Replace(":", "-");
+
+            if (!Directory.Exists($"Results\\{currentCheckPath}\\Countries"))
+                Directory.CreateDirectory($"Results\\{currentCheckPath}\\Countries");
+
+            proxy
+                .ForEach(px =>
+                {
+                    
+                    using (var stream = File.AppendText($"Results\\{currentCheckPath}\\Alive.txt"))
+                        stream.WriteLineAsync(px?.Proxy);
+                    
+                    var code = px?.Country?.IsoCode;
+                    using (var stream = File.AppendText($"Results\\{currentCheckPath}\\Countries\\{code}.txt"))
+                        stream.WriteLineAsync(px?.Proxy);
+                });
+            
+            return Task.CompletedTask;
+        }
+    }
+}
